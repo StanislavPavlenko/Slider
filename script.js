c6bc5c22c2f77ba179322ccf0f295e517d815656
StanislavPavlenko <voltfrag@gmail.com>
2023-03-18 23:52:32 +0200

    213

---
+++ script.js
@@ -0,0 +1,12 @@
+new Swiper('.list-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoHeight: true,
+    sliderPerView: 3,
+});
